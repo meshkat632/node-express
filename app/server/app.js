5c18bd9f184f3984fbe183a5909ddffbc3a06f11
meshkat <meshkat632@gmail.com>
2018-07-18 19:01:53 +0200

    initial folder structure comepleted

--- app/server/app.js
+++ app/server/app.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
+import path from 'path';
+import express from 'express';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const publicPath = express.static(path.join(__dirname, '../'));
+const indexPath = path.join(__dirname, '../index.html');
+
+app.use(publicPath);
+
+app.get('/', (req, res) => {
+    res.sendFile(indexPath);
+})
+
+export default app;
